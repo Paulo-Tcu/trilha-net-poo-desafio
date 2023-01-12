2c754a4f0a670a29f0eaf251875bacd008142310
Paulo Cesar Querino Da Cruz De Araujo <paulo.cruz@linx.com.br>
2023-01-12 04:05:35 -0300

    desafio de projeto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,25 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string _modelo;
+        protected string _imei;
+        protected int _memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+            
+            if(string.IsNullOrEmpty(numero) || string.IsNullOrEmpty(modelo) || string.IsNullOrEmpty(imei))
+                    throw new ArgumentException();
+
+            if(memoria <= 0)
+                throw new ArgumentException("Parâmetro inválido",nameof(memoria));
+
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
+
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("1234","nokia 01", "123456789", 4);
+
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("jogo 1");
+
+var iphone = new Iphone("4321","iphone teste", "987654321", 6);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
